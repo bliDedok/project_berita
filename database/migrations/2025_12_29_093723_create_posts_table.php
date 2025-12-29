d0217c39a98ab41754f3ac277f0b39cb7dc98f7e
Dedy Wanditya <dedywandityalover@gmail.com>
2025-12-29 18:18:34 +0800

    Membuat tabel kategori dan post berita menggunakan migrasi laravel

---
+++ database/migrations/2025_12_29_093723_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->string('slug')->unique();
+            $table->string('ringkasan');
+            $table->string('konten');
+            $table->string('image');
+            $table->foreignId('kategori_id');
+            $table->foreignId('user_id');
+            $table->timestamp('tanggal_unggah');
+            $table->integer('jumlah_pembaca');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
