b09fe97e5d7d186af5dab593c80d0fed0b62953a
Gdeliemena <gdeliem003@gmail.com>
2025-12-29 22:28:27 +0800

    feat: menambahkan routing resource post untuk admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\PostController as AdminPostController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')
+    
+    ->group(function () {
+        Route::resource('posts', AdminPostController::class)->names('admin.posts');
+    });
